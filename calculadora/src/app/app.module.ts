1f26c22d2724ce0dba17554777244a2c55fdf1f9
laosarah <139395413+laosarah@users.noreply.github.com>
2023-11-21 18:01:07 -0300

    T001-PAINEL: chore - criação do componente painel

--- calculadora/src/app/app.module.ts
+++ calculadora/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculadoraComponent } from './components/calculadora/calculadora.component';
+import { PainelComponent } from './components/calculadora/painel/painel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculadoraComponent
+    CalculadoraComponent,
+    PainelComponent
   ],
   imports: [
     BrowserModule,
